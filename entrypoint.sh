dbcf1e507030e6625685a5425700cb0f5fde6f9a
Karsten Hachmeister <karsten@hachmeister.org>
2021-10-06 14:10:52 +0200

    add shutdown handler to save world

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,17 @@ BUILD="304"
 FILENAME="paper-${VERSION}-${BUILD}.jar"
 URL="https://papermc.io/api/v2/projects/paper/versions/${VERSION}/builds/${BUILD}/downloads/${FILENAME}"
 
+shutdown_handler() {
+  rcon-cli --config /data/.rcon-cli.yaml say "Shutdown in 10 seconds!"
+
+  sleep 10
+
+  rcon-cli --config /data/.rcon-cli.yaml save-all
+  rcon-cli --config /data/.rcon-cli.yaml stop
+}
+
+trap 'shutdown_handler' SIGTERM
+
 cd /data
 
 if [ ! -f "server.properties" ]; then
@@ -21,4 +32,7 @@ fi
 
 echo "starting minecraft with ${FILENAME}..."
 JAVA_OPTS="-Xms2G -Xmx2G"
-java $JAVA_OPTS -jar "${FILENAME}" --nogui
+java $JAVA_OPTS -jar "${FILENAME}" --nogui &
+pid="$!"
+
+wait "$pid"
