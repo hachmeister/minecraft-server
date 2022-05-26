72a05341cc0c5e8779719151a868c53b9bd4600a
Karsten Hachmeister <karsten@hachmeister.org>
2022-05-26 18:55:29 +0200

    papermc - version 1.18.2 - build 347

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="1.18.2"
-BUILD="317"
+BUILD="347"
 FILENAME="paper-${VERSION}-${BUILD}.jar"
 URL="https://papermc.io/api/v2/projects/paper/versions/${VERSION}/builds/${BUILD}/downloads/${FILENAME}"
 
